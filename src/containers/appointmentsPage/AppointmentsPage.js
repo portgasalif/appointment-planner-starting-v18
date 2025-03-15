0c5722b05762d910c9fdda1ffc06595f7c5c074f
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-03-15 14:11:07 +0700

    Enhance AppointmentsPage and Tile components with props for better data handling

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,28 +3,60 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = () => {
-  /*
-  Define state variables for 
-  appointment info
-  */
+export const AppointmentsPage = ({
+  contacts,
+  appointments,
+  addAppointment,
+}) => {
+  const [appointmentName, setAppointmentName] = useState("");
+  const [appointmentContact, setAppointmentContact] = useState("");
+  const [appointmentDate, setAppointmentDate] = useState("");
+  const [appointmentTime, setAppointmentTime] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data  
-    */
-   
+    if (
+      !appointmentName ||
+      !appointmentContact ||
+      !appointmentDate ||
+      !appointmentTime
+    ) {
+      alert("Please fill in all fields");
+      return;
+    }
+    addAppointment(
+      appointmentName,
+      appointmentContact,
+      appointmentDate,
+      appointmentTime
+    );
+    setAppointmentName("");
+    setAppointmentContact("");
+    setAppointmentDate("");
+    setAppointmentTime("");
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm
+          appointmentName={appointmentName}
+          appointmentContact={appointmentContact}
+          appointmentDate={appointmentDate}
+          appointmentTime={appointmentTime}
+          setAppointmentName={setAppointmentName}
+          setAppointmentContact={setAppointmentContact}
+          setAppointmentDate={setAppointmentDate}
+          setAppointmentTime={setAppointmentTime}
+          contacts={contacts}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList tiles={appointments} />
       </section>
     </div>
   );
