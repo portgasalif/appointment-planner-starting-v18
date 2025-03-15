535f718dc3d85736ff1e763b788f7b2d3da51f88
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-03-15 16:39:05 +0700

    Refactor AppointmentForm and ContactPicker components for improved state management and user interaction

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,26 +1,63 @@
 import React from "react";
-
+import { ContactPicker } from "../contactPicker/ContactPicker";
 const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
+  const [month, day, year] = new Date().toLocaleDateString("en-US").split("/");
   return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
 };
 
 export const AppointmentForm = ({
   contacts,
-  title,
-  setTitle,
-  contact,
-  setContact,
-  date,
-  setDate,
-  time,
-  setTime,
-  handleSubmit
+  appointmentName,
+  setAppointmentName,
+  appointmentContact,
+  setAppointmentContact,
+  appointmentDate,
+  setAppointmentDate,
+  appointmentTime,
+  setAppointmentTime,
+  handleSubmit,
 }) => {
-
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={appointmentName}
+            onChange={(e) => setAppointmentName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Contact:
+          <ContactPicker
+            value={appointmentContact}
+            onChange={(e) => setAppointmentContact(e.target.value)}
+            contacts={contacts}
+          />
+        </label>
+        <label>
+          Date:
+          <input
+            type="date"
+            value={appointmentDate}
+            onChange={(e) => setAppointmentDate(e.target.value)}
+            min={getTodayString()}
+            required
+          />
+        </label>
+        <label>
+          Time:
+          <input
+            type="time"
+            value={appointmentTime}
+            onChange={(e) => setAppointmentTime(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Add Appointment</button>
+      </form>
+    </>
   );
 };
--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <>
+      <select name={name} value={value} onChange={onChange}>
+        <option value="">No Contact Selected</option>
+        {contacts.map((contact, index) => {
+          return (
+            <option key={index} value={contact.name}>
+              {contact.name}
+            </option>
+          );
+        })}
+      </select>
+    </>
   );
 };
